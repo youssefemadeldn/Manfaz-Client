48a9447c47211de37f5a59c2cd8d619ff8242844
youssef emad eldin <youssefemadeldn>
2025-01-28 14:36:22 +0200

    init google maps

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:manfaz/core/routes/routes.dart';
 import 'package:manfaz/features/bottom_navigation_bar/presentation/view/cus_bottom_navigation_bar.dart';
 import 'package:manfaz/features/login/presentation/view/login_view.dart';
@@ -6,9 +6,8 @@ import 'package:manfaz/features/login/presentation/view/otp_verification_view.da
 import 'package:manfaz/features/login/presentation/view/success_verification_view.dart';
 import 'package:manfaz/features/onBoardings/language_switcher_view.dart';
 import 'package:manfaz/features/onBoardings/presentation/views/on_boarding_view.dart';
-// import 'package:manfaz/features/onBoardings/presentation/views/your_turn_view.dart';
-
 import '../../features/freelancer_profile/presentation/views/freelancer_profile_view.dart';
+import '../../features/google_maps/presentation/views/get_user_location_view.dart';
 import '../../features/people_list_view/presentation/views/people_list_view_view.dart';
 import '../../features/register/presentation/views/register_view.dart';
 import '../../features/send_an_offer/presentation/views/send_an_offer_view.dart';
@@ -17,38 +16,40 @@ class AppRouter {
   static Route generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.onBoarding:
-        return MaterialPageRoute(builder: (_) => const OnBoardingView());
+        return CupertinoPageRoute(builder: (_) => const OnBoardingView());
 
       // case Routes.yourTurnView:
-      //   return MaterialPageRoute(builder: (_) => const YourTurnView());
+      //   return CupertinoPageRoute(builder: (_) => const YourTurnView());
 
       case Routes.loginView:
-        return MaterialPageRoute(builder: (_) => const LoginView());
+        return CupertinoPageRoute(builder: (_) => const LoginView());
 
       case Routes.registerView:
-        return MaterialPageRoute(builder: (_) => const RegisterView());
+        return CupertinoPageRoute(builder: (_) => const RegisterView());
       case Routes.successVerificationView:
-        return MaterialPageRoute(
+        return CupertinoPageRoute(
             builder: (_) => const SuccessVerificationView());
       case Routes.otpVerificationView:
-        return MaterialPageRoute(builder: (_) => const OtpVerificationView());
+        return CupertinoPageRoute(builder: (_) => const OtpVerificationView());
       case Routes.cusBottomNavigationBar:
-        return MaterialPageRoute(builder: (_) => CusBottomNavigationBar());
+        return CupertinoPageRoute(builder: (_) => CusBottomNavigationBar());
 
       case Routes.peopleListViewView:
-        return MaterialPageRoute(builder: (_) => PeopleListViewView());
+        return CupertinoPageRoute(builder: (_) => PeopleListViewView());
 
       case Routes.freelancerProfileView:
-        return MaterialPageRoute(builder: (_) => FreelancerProfileView());
+        return CupertinoPageRoute(builder: (_) => FreelancerProfileView());
 
       case Routes.sendAnOfferView:
-        return MaterialPageRoute(builder: (context) => SendAnOfferView());
+        return CupertinoPageRoute(builder: (context) => SendAnOfferView());
       case Routes.languageSwitcherScreen:
-        return MaterialPageRoute(builder: (context) => LanguageSwitcherView());
+        return CupertinoPageRoute(builder: (context) => LanguageSwitcherView());
+      case Routes.getUserLocationView:
+        return CupertinoPageRoute(builder: (context) => GetUserLocationView());
       default:
-        return MaterialPageRoute(
-          builder: (_) => Scaffold(
-            body: Center(
+        return CupertinoPageRoute(
+          builder: (_) => CupertinoPageScaffold(
+            child: Center(
               child: Text("No Route defined for ${settings.name}"),
             ),
           ),
